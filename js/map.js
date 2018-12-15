72d19bc7dba48a8f25b13eb78618999ba393e0e1
yevheniia <evgesha.drozdova@gmail.com>
2018-12-15 19:44:17 +0200

    додала фільтр по конфліктам

--- js/map.js
+++ js/map.js
@@ -3,6 +3,7 @@ var width = rect.width,
     height = rect.height;
 
 
+var selectedRegion;
 
 var projection = d3.geoMercator()
     .scale(15000)
@@ -21,6 +22,9 @@ var map = d3.select("#mapid")
 
 d3.json("data/map.geojson", drawMaps);
 
+
+
+
 function drawMaps(geojson) {
     map.selectAll("path")
         .data(geojson.features)
@@ -34,155 +38,167 @@ function drawMaps(geojson) {
         .on("click", function(d) {
         $(".region").attr("fill", "lightgrey");
             $(this).attr("fill", "red");
-            var selectedRegion = d.properties.VARNAME_2;
-            var selectedType = $( "#select option:selected").val();
-
+            selectedRegion = d.properties.VARNAME_2;
             var container = d3.select("#cases");
-
-
 
         d3.csv("data/data.csv", function(mydata){
 
-            var regionData = mydata.filter(function(d){
-                if(selectedType === "") {
-                    return d.district === selectedRegion;
-                } else {
-                    return d.district === selectedRegion && d.type === selectedType;
-                }
-            });
+            drawCases(mydata, selectedRegion);
 
-            $("#cases").html("");
-
-
-            d3.select("#cases")
-                .append("h1")
-                .html(selectedRegion + " район");
-
-           var cases = d3.select("#cases")
-                .selectAll("div")
-                .data(regionData)
-                .enter()
-                .append("div")
-                .attr("class","cases")
-               ;
-
-            cases.each(function(d) {
-                d3.select(this).style("background-color", function(d) {
-                    if(d.level === "помірно гострий"){
-                        return "yellow"
-                    } else if (d.level === "гострий"){
-                        return "orange"
-                    } else if (d.level === "негострий"){
-                        return "lightgrey"
-                    }
-
-                });
-
-                d3.select(this)
-                    .append("p")
-                    .attr("class", "title")
-                    .text(function(d) {
-                        return d.summary
-                    });
-
-                var indicators = d3.select(this)
-                    .append("div")
-                    .attr("id", "indicators-container");
-
-                indicators.append("img")
-                    .attr("class", "indicators")
-                    .attr("id", "hate-speech")
-                    .attr("src", "img/1.png")
-                    .attr("title", "мова ворожнечі в статті")
-                    .style("opacity", function(d) {
-                        if(d.hatespeech === "TRUE"){
-                            return 1
-                        } else {
-                            return 0.1
-                        }
-                    });
-
-                indicators.append("img")
-                    .attr("class", "indicators")
-                    .attr("id", "hate-speech")
-                    .attr("src", "img/2.png")
-                    .attr("title", "заклики до ворожнечі")
-                    .style("opacity", function(d) {
-                        if(d.hatespeech_call === "TRUE"){
-                            return 1
-                        } else {
-                            return 0.1
-                        }
-                    });
-
-                indicators.append("img")
-                    .attr("class", "indicators")
-                    .attr("id", "hate-speech")
-                    .attr("src", "img/3.png")
-                    .attr("title", "мова ворочнечі в коментарях до статті")
-                    .style("opacity", function(d) {
-                        console.log(d);
-                        if(d.hatespeech_comments === "TRUE"){
-                            return 1
-                        } else {
-                            return 0.1
-                        }
-                    });
-
-                indicators.append("img")
-                    .attr("class", "indicators")
-                    .attr("id", "hate-speech")
-                    .attr("src", "img/court.png")
-                    .attr("title", function(d) { return d.court_string })
-                    .style("opacity", function(d) {
-                        if(d.court_logical=== "TRUE"){
-                            return 1
-                        } else {
-                            return 0.1
-                        }
-                    });
-
-                indicators.append("img")
-                    .attr("class", "indicators")
-                    .attr("id", "hate-speech")
-                    .attr("src", "img/appeal.png")
-                    .attr("title", function(d) { return d.appeal })
-                    .style("opacity", function(d) {
-                        if(d.appeal_logical=== "TRUE"){
-                            return 1
-                        } else {
-                            return 0.1
-                        }
-                    });
-                
-                    
-
-                d3.select(this)
-                    .append("p")
-                    .attr("class", "description")
-                    .attr("display", "block")
-                    .text(function(d) {
-                        return d.description
-                    });
-
-                
-
-                d3.select(this)
-                    .append("div")
-                    .attr("class", "links")
-                    .attr("display", "block")
-                    .html(function(d) {
-                        return d.links
-                    });
-            })
         });
-
-
     });
 
 }
 
 
+$('select').on('change', function() {
+    d3.csv("data/data.csv", function(mydata){
+
+        drawCases(mydata, selectedRegion);
+
+    });
+});
 
 
-//Load in GeoJSON data
+
+
+
+
+
+var drawCases = function(df, region) {
+    var selectedType = $( "#select option:selected").val();
+    var regionData = df.filter(function(d){
+        if(selectedType === "") {
+            return d.district === selectedRegion;
+        } else {
+            return d.district === selectedRegion && d.type === selectedType;
+        }
+    });
+
+    $("#cases").html("");
+
+
+    d3.select("#cases")
+        .append("h1")
+        .html(selectedRegion + " район");
+
+    var cases = d3.select("#cases")
+            .selectAll("div")
+            .data(regionData)
+            .enter()
+            .append("div")
+            .attr("class", "cases")
+        ;
+
+    cases.each(function (d) {
+        d3.select(this).style("background-color", function (d) {
+            if (d.level === "помірно гострий") {
+                return "yellow"
+            } else if (d.level === "гострий") {
+                return "orange"
+            } else if (d.level === "негострий") {
+                return "lightgrey"
+            }
+
+        });
+
+        d3.select(this)
+            .append("p")
+            .attr("class", "title")
+            .text(function (d) {
+                return d.summary
+            });
+
+        var indicators = d3.select(this)
+            .append("div")
+            .attr("id", "indicators-container");
+
+        indicators.append("img")
+            .attr("class", "indicators")
+            .attr("id", "hate-speech")
+            .attr("src", "img/1.png")
+            .attr("title", "мова ворожнечі в статті")
+            .style("opacity", function (d) {
+                if (d.hatespeech === "TRUE") {
+                    return 1
+                } else {
+                    return 0.1
+                }
+            });
+
+        indicators.append("img")
+            .attr("class", "indicators")
+            .attr("id", "hate-speech")
+            .attr("src", "img/2.png")
+            .attr("title", "заклики до ворожнечі")
+            .style("opacity", function (d) {
+                if (d.hatespeech_call === "TRUE") {
+                    return 1
+                } else {
+                    return 0.1
+                }
+            });
+
+        indicators.append("img")
+            .attr("class", "indicators")
+            .attr("id", "hate-speech")
+            .attr("src", "img/3.png")
+            .attr("title", "мова ворочнечі в коментарях до статті")
+            .style("opacity", function (d) {
+                console.log(d);
+                if (d.hatespeech_comments === "TRUE") {
+                    return 1
+                } else {
+                    return 0.1
+                }
+            });
+
+        indicators.append("img")
+            .attr("class", "indicators")
+            .attr("id", "hate-speech")
+            .attr("src", "img/court.png")
+            .attr("title", function (d) {
+                return d.court_string
+            })
+            .style("opacity", function (d) {
+                if (d.court_logical === "TRUE") {
+                    return 1
+                } else {
+                    return 0.1
+                }
+            });
+
+        indicators.append("img")
+            .attr("class", "indicators")
+            .attr("id", "hate-speech")
+            .attr("src", "img/appeal.png")
+            .attr("title", function (d) {
+                return d.appeal
+            })
+            .style("opacity", function (d) {
+                if (d.appeal_logical === "TRUE") {
+                    return 1
+                } else {
+                    return 0.1
+                }
+            });
+
+
+        d3.select(this)
+            .append("p")
+            .attr("class", "description")
+            .attr("display", "block")
+            .text(function (d) {
+                return d.description
+            });
+
+
+        d3.select(this)
+            .append("div")
+            .attr("class", "links")
+            .attr("display", "block")
+            .html(function (d) {
+                return d.links
+            });
+    })
+};
