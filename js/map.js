075623cd2eabd0b0dcc9d4dcc87ea45086f83400
yevheniia <evgesha.drozdova@gmail.com>
2019-01-07 00:33:44 +0200

    додала прапорці

--- js/map.js
+++ js/map.js
@@ -8,24 +8,44 @@ var selectedRegion;
 var projection = d3.geoMercator()
     .scale(15000)
     // .rotate([-0.25, 0.25, 0])
-    .center([29.5, 45.8]);
+    .center([29.2, 46.0]);
 
 
-var path = d3.geoPath().projection(projection);
+var path = d3.geoPath()
+    .projection(projection);
 
+var path2 = d3.geoPath()
+    .projection(projection);
 
 var map = d3.select("#mapid")
     .append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .attr("id", "bessarabia");
+
+var map2 = d3.select("#mapid")
+    .insert("svg", "#bessarabia")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("id", "ukraine");
 
 var markers = map.append("g");
 
+d3.json("data/ukr_shape.geojson", drawUkraine);
+
+function drawUkraine(ukraine) {
+    map2.selectAll("path")
+        .data(ukraine.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("fill", "#F3F3F3")
+        .attr("opacity", 0.05)
+
+}
+
 
 d3.json("data/map.geojson", drawMaps);
-
-
-
 
 function drawMaps(geojson) {
     map.selectAll("path")
@@ -36,7 +56,7 @@ function drawMaps(geojson) {
         .attr("d", path)
         .attr("fill", "lightgrey")
         .attr("fill-opacity", 0.9)
-        .attr("stroke", "#222")
+        .attr("stroke", "#1d1d1d")
         .on("click", function(d) {
         $(".region").attr("fill", "lightgrey");
             $(this).attr("fill", "red");
@@ -83,7 +103,6 @@ $('select').on('change', function() {
 
 
 
-
 var drawCases = function(df, region) {
     var selectedType = $( "#select option:selected").val();
     var regionData = df.filter(function(d){
@@ -94,14 +113,14 @@ var drawCases = function(df, region) {
         }
     });
 
-    $("#cases").html("");
+    $("#cases-container").html("");
+    $("#details").css("display", "none");
 
 
-    d3.select("#cases")
-        .append("h1")
+    d3.select("#paste-region")
         .html(selectedRegion + " район");
 
-    var cases = d3.select("#cases")
+    var cases = d3.select("#cases-container")
             .selectAll("div")
             .data(regionData)
             .enter()
@@ -124,8 +143,8 @@ var drawCases = function(df, region) {
                 .attr("width", 30)
                 .attr("height", 30)
                 .attr("class", "mark")
-                // .attr("xlink:href",'img/pin.png')
-                .attr("xlink:href",'https://cdn3.iconfinder.com/data/icons/softwaredemo/PNG/24x24/DrawingPin1_Blue.png')
+                .attr("xlink:href",'img/pin.png')
+                // .attr("xlink:href",'https://cdn3.iconfinder.com/data/icons/softwaredemo/PNG/24x24/DrawingPin1_Blue.png')
 
                 .attr("x", function () {
                     console.log(projection(marks));
@@ -135,9 +154,9 @@ var drawCases = function(df, region) {
                 })
 
         })
-        //     .on("mouseout", function() {
-        //     $(".mark").remove();
-        // })
+            .on("mouseout", function() {
+            $(".mark").remove();
+        })
         ;
 
 
